b459fade67be2034aea1175bcf2f57e1801d1185
Victor Garcia <vmgarcia12@gmail.com>
2021-12-02 15:20:00 -0800

    Solved problem 4 (day 2 part 2)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod problem_1;
 mod problem_2;
 mod problem_3;
+mod problem_4;
 
 fn main() {
     match problem_1::problem_1() {
@@ -21,7 +22,15 @@ fn main() {
     }
     match problem_3::problem_3() {
         Ok(result) => {
-            println!("Problem 2: {:?}", result);
+            println!("Problem 3: {:?}", result);
+        }
+        Err(err) => {
+            println!("There was an error! {:?}", err);
+        }
+    }
+    match problem_4::problem_4() {
+        Ok(result) => {
+            println!("Problem 4: {:?}", result);
         }
         Err(err) => {
             println!("There was an error! {:?}", err);
---
+++ src/problem_4.rs
@@ -0,0 +1,35 @@
+use std::fs;
+use std::io;
+use std::io::prelude::*;
+use std::path::Path;
+
+pub fn problem_4() -> io::Result<u64> {
+    let path_to_read = Path::new("./src/day-2-problem-3-input.txt");
+    let file = fs::File::open(&path_to_read)?;
+    let reader = io::BufReader::new(file);
+
+    let mut position = (0, 0, 0);
+    for line_result in reader.lines() {
+        let line = line_result?;
+        let mut line = line.split_whitespace();
+        if let (Some(direction), Some(distance_str)) = (line.next(), line.next()) {
+            if let Ok(distance) = distance_str.parse::<u64>() {
+                match direction {
+                    "forward" => {
+                        position.0 += distance;
+                        position.1 += position.2 * distance;
+                    },
+                    "down" => {
+                        position.2 += distance;
+                    },
+                    "up" => {
+                        position.2 -= distance;
+                    },
+                    _ => ()
+                }
+            }
+        }
+    }
+
+    Ok(position.0 * position.1)
+}
