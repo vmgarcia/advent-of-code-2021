25addd3d0d0193625236085095c0b6e15d2c71e8
Victor Garcia <vmgarcia12@gmail.com>
2021-12-02 15:11:10 -0800

    Solved problem 3 on day 2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod problem_1;
 mod problem_2;
+mod problem_3;
 
 fn main() {
     match problem_1::problem_1() {
@@ -11,6 +12,14 @@ fn main() {
         }
     }
     match problem_2::problem_2() {
+        Ok(result) => {
+            println!("Problem 2: {:?}", result);
+        }
+        Err(err) => {
+            println!("There was an error! {:?}", err);
+        }
+    }
+    match problem_3::problem_3() {
         Ok(result) => {
             println!("Problem 2: {:?}", result);
         }
---
+++ src/problem_3.rs
@@ -0,0 +1,34 @@
+use std::fs;
+use std::io;
+use std::io::prelude::*;
+use std::path::Path;
+
+pub fn problem_3() -> io::Result<u64> {
+    let path_to_read = Path::new("./src/day-2-problem-3-input.txt");
+    let file = fs::File::open(&path_to_read)?;
+    let reader = io::BufReader::new(file);
+
+    let mut position = (0, 0);
+    for line_result in reader.lines() {
+        let line = line_result?;
+        let mut line = line.split_whitespace();
+        if let (Some(direction), Some(distance_str)) = (line.next(), line.next()) {
+            if let Ok(distance) = distance_str.parse::<u64>() {
+                match direction {
+                    "forward" => {
+                        position.0 += distance;
+                    },
+                    "down" => {
+                        position.1 += distance;
+                    },
+                    "up" => {
+                        position.1 -= distance;
+                    },
+                    _ => ()
+                }
+            }
+        }
+    }
+
+    Ok(position.0 * position.1)
+}
